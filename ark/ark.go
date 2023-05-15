f25acca38ef0556fc4a12e0a961d6c7186d82182
Jokin <327928971@qq.com>
2023-05-15 13:47:32 +0800

    :rabbit2: Update

--- ark/ark.go
+++ ark/ark.go
@@ -1,9 +1,41 @@
 package ark
 
 import (
+	"os"
+
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func init() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+}
+
+func Panic() *zerolog.Event {
+	return log.Panic()
+}
+
+func Fatal() *zerolog.Event {
+	return log.Fatal()
+}
+
+func Error() *zerolog.Event {
+	return log.Error()
+}
+
+func Warn() *zerolog.Event {
+	return log.Warn()
+}
+
+func Info() *zerolog.Event {
+	return log.Info()
+}
+
+func Debug() *zerolog.Event {
+	return log.Debug()
+}
+
+func Trace() *zerolog.Event {
+	return log.Trace()
 }
--- ark/ark_test.go
+++ ark/ark_test.go
@@ -3,11 +3,13 @@ package ark_test
 import (
 	"testing"
 
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	"github.com/jokin1999/certark/ark"
 )
 
 func TestLog(T *testing.T) {
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	log.Info().Msg("Hello World")
+	ark.Trace().Msg("this is trace")
+	ark.Debug().Msg("this is debug")
+	ark.Info().Str("key", "value").Msg("this is info")
+	ark.Warn().Msg("this is warn")
+	ark.Error().Msg("this is error")
 }
